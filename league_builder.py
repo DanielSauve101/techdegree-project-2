e3d09cb315c7d99a5efab1addc4518f0dcb8df10
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-02-19 10:40:59 -0500

    included in the write_team_info function a for loop to write individual letters to each guardian

--- league_builder.py
+++ league_builder.py
@@ -20,36 +20,36 @@ def player_info_list():
 
 
 def sort_players_by_experience(player_info, type_of_player):
-    experienced_player = []
-    beginner_player = []
+    experienced_players = []
+    beginner_players = []
 
     for player in player_info:
         for experience in player:
             if experience == "YES":
-                experienced_player.append(player)
+                experienced_players.append(player)
             elif experience == "NO":
-                beginner_player.append(player)
+                beginner_players.append(player)
 
     if type_of_player == "experienced":
-        return experienced_player
+        return experienced_players
     elif type_of_player == "beginner":
-        return beginner_player
+        return beginner_players
 
 
-def create_team(team, experienced_player, beginner_player):
-    exp_num_per_team = int(len(experienced_player) / 3)
-    beg_num_per_team = int(len(beginner_player) / 3)
+def create_team(team, experienced_players, beginner_players):
+    exp_num_per_team = int(len(experienced_players) / 3)
+    beg_num_per_team = int(len(beginner_players) / 3)
 
-    if team == "Sharks":
-        team = experienced_player[:exp_num_per_team] + beginner_player[:beg_num_per_team]
+    if team == "team_one":
+        team = experienced_players[:exp_num_per_team] + beginner_players[:beg_num_per_team]
         return team
-    elif team == "Dragons":
-        team = experienced_player[exp_num_per_team:(
-            exp_num_per_team*2)] + beginner_player[beg_num_per_team:(beg_num_per_team*2)]
+    elif team == "team_two":
+        team = experienced_players[exp_num_per_team:(
+            exp_num_per_team*2)] + beginner_players[beg_num_per_team:(beg_num_per_team*2)]
         return team
-    elif team == "Raptors":
-        team = experienced_player[-1:(exp_num_per_team*2-1):-1] + \
-            beginner_player[-1:(beg_num_per_team*2-1):-1]
+    elif team == "team_three":
+        team = experienced_players[-1:(exp_num_per_team*2-1):-1] + \
+            beginner_players[-1:(beg_num_per_team*2-1):-1]
         return team
 
 
@@ -61,14 +61,21 @@ def write_team_info(team, title):
             file.write(player_name + ", " + player_experience + ", " + player_guardian + "\n")
         file.write("\n")
 
+    for player_name, player_experience, player_guardian in team:
+        players = player_name.lower().replace(" ", "_")
+        with open(players + ".txt", "w") as file:
+            file.write("Dear " + player_guardian + ",\n\n" + "We would like to welcome " +
+                       player_name + " to the " + title + " team." +
+                       "\nThe first practice is May 25th, 2019 at 2:00pm.")
+
 
 def create_soccer_league():
     player_info = player_info_list()
-    beginner_player = sort_players_by_experience(player_info, "beginner")
-    experienced_player = sort_players_by_experience(player_info, "experienced")
-    Sharks = create_team("Sharks", experienced_player, beginner_player)
-    Dragons = create_team("Dragons", experienced_player, beginner_player)
-    Raptors = create_team("Raptors", experienced_player, beginner_player)
+    beginner_players = sort_players_by_experience(player_info, "beginner")
+    experienced_players = sort_players_by_experience(player_info, "experienced")
+    Sharks = create_team("team_one", experienced_players, beginner_players)
+    Dragons = create_team("team_two", experienced_players, beginner_players)
+    Raptors = create_team("team_three", experienced_players, beginner_players)
     with open("teams.txt", "w") as file:
         write_team_info(Sharks, "Sharks")
         write_team_info(Dragons, "Dragons")
